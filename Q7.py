4ef9d9eb6f887e0e6b55d05ffe987298276f347e
Vedant Dange <vedantdange35@gmail.com>
2026-07-02 12:55:05 +0530

    Arithmetic Operations

---
+++ Q7.py
@@ -0,0 +1,24 @@
+#Q.7)
+import numpy as np
+a = np.array([10, 20, 30, 40])
+b = np.array([1, 2, 3, 4])
+print("Array a:", a)
+print("Array b:", b)
+# Addition
+print("\nAddition:")
+print(a + b)
+# Subtraction
+print("\nSubtraction:")
+print(a - b)
+# Multiplication
+print("\nMultiplication:")
+print(a * b)
+# Division
+print("\nDivision:")
+print(a / b)
+# Element-wise Power
+print("\nElement-wise Power:")
+print(a ** b)
+# Modulo Operation
+print("\nModulo Operation:")
+print(a % b)
